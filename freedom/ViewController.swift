e2f3168f164d58addc9ecacc92d9576517fe717e
morning <morning@thecover.co>
2017-03-20 10:04:44 +0800

    update

--- freedom/ViewController.swift
+++ freedom/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
 
@@ -15,6 +16,19 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let animation = LOTAnimationView.animationNamed("kkkkkk")
+        animation?.contentMode = .scaleAspectFit
+        self.view.addSubview(animation!)
+        animation?.frame = CGRect.init(x: 0, y: 0, width: 320, height: 400)
+        animation?.play(completion: { (finished) in
+            //
+            
+        })
+        
+
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
